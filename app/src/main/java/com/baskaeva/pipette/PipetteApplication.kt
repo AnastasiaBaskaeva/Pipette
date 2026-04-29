9d1395b9c62edf7267a73890a3f00c989cbbf7ae
awwauevich <bnastua10.2006@gmail.com>
2026-04-29 16:29:50 +0300

    project structure

---
+++ app/src/main/java/com/baskaeva/pipette/PipetteApplication.kt
@@ -0,0 +1,6 @@
+package com.baskaeva.pipette
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+class PipetteApplication : Application()
---
+++ app/src/main/java/com/baskaeva/pipette/di/AppModules.kt
@@ -0,0 +1,1 @@
+package com.baskaeva.pipette.di
